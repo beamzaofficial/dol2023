ef996d333e449ae2c49170f201c7136836c02c73
Attapon Sroysuwan <55727960+eikqzx@users.noreply.github.com>
2023-01-13 12:17:17 +0700

    Q

--- pages/components/appbar.js
+++ pages/components/appbar.js
@@ -1,13 +1,16 @@
-import { Menu } from '@mui/icons-material'
-import { Box, Button, Drawer, Grid, IconButton, Toolbar, Typography } from '@mui/material'
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import { Box, Button, Divider, Drawer, Grid, IconButton, TextField, Toolbar, Typography } from '@mui/material'
 import MuiAppBar from '@mui/material/AppBar';
-import Head from 'next/head'
 import React from 'react'
 import Image from 'next/image'
-import DrawerLogin from './drawerLogin'
 import { styled, useTheme } from '@mui/material/styles';
+import dayjs from 'dayjs'
+import buddhistEra from "dayjs/plugin/buddhistEra";
+dayjs.extend(buddhistEra);
 
 const drawerWidth = 360;
+
 const AppBar = styled(MuiAppBar, {
     shouldForwardProp: (prop) => prop !== 'open',
 })(({ theme, open }) => ({
@@ -25,21 +28,30 @@ const AppBar = styled(MuiAppBar, {
     }),
 }));
 
-export default function Appbar() {
-    const [openSideBar, setOpenSideBar] = React.useState(false);
+const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+}));
 
-    const onClickOpen = () => {
-        setOpenSideBar(true);
-    }
+export default function Appbar(props) {
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+    const [showTime,setShowTime] = React.useState("");
+    // const [directionIcon, setDirectionIcon] = React.useState(<ChevronLeftIcon />);
+    const theme = useTheme();
 
-    const onCloseOpen = () => {
-        setOpenSideBar(false);
-    }
+    React.useEffect(()=>{
+        setShowTime(dayjs().format("DD/MM/BBBB HH:mm:ss"));
+    },[])
 
     return (
         <Box sx={{ display: 'flex' }}>
             <Grid>
-                <AppBar color="inherit" position="fixed" open={openSideBar}>
+                <AppBar color="inherit" position="fixed" open={props.openSideBar}>
                     <Grid p={1}>
                         <Toolbar>
                             <Image src={"/samartcorp.png"} width={150} height={50} />
@@ -50,11 +62,56 @@ export default function Appbar() {
                             <Image src={"/logoLOD.png"} width={150} height={50} />
                             <Typography color={"black"} variant="h6" component="div" sx={{ flexGrow: 1 }}>
                             </Typography>
-                            <Button sx={{ ...(openSideBar && { display: 'none' }) }} onClick={onClickOpen}>Login</Button>
+                            <Button sx={{ ...(props.openSideBar && { display: 'none' }) }} onClick={props.onClickOpen}>Login</Button>
                         </Toolbar>
                     </Grid>
                 </AppBar>
-                <DrawerLogin onOpen={openSideBar} onClose={onCloseOpen} />
+                <Drawer
+                    sx={{
+                        width: drawerWidth,
+                        flexShrink: 0,
+                        '& .MuiDrawer-paper': {
+                            width: drawerWidth,
+                            boxSizing: 'border-box',
+                        },
+                    }}
+                    variant="persistent"
+                    anchor="right"
+                    open={props.openSideBar}
+                >
+                    <DrawerHeader>
+                        <Grid container justifyContent={"center"}>
+                            <Grid item>
+                                <Typography variant='h6'>Admin Login</Typography>
+                            </Grid>
+                        </Grid>
+                        <IconButton onClick={props.onCloseOpen}>
+                            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                        </IconButton>
+                    </DrawerHeader>
+                    <Divider />
+                    <Grid container justifyContent={"center"} p={2}>
+                        <Grid item py={1}>
+                            <Typography>{showTime}</Typography>
+                        </Grid>
+                        <Grid item py={1}>
+                            <TextField size='small' error={username == ""} label="Username" variant="outlined" required value={username} onChange={(e) => { setUsername(e) }} />
+                        </Grid>
+                        <Grid item py={1}>
+                            <TextField size='small' error={password == ""} label="Password" variant="outlined" required value={password} onChange={(e) => { setPassword(e) }} />
+                        </Grid>
+                        <Grid item py={1}>
+                            <Grid container spacing={2}>
+                                <Grid item>
+                                    <Button variant='contained' color='info'>Login</Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='contained' color='error'>Logout</Button>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Drawer>
             </Grid>
         </Box>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,48 @@
-import { Grid } from '@mui/material'
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '../styles/Home.module.css'
 import Appbar from './components/appbar'
 import DrawerLogin from './components/drawerLogin'
+import React from 'react'
+import { styled, useTheme } from '@mui/material/styles';
 
-export default function Home() {
+const drawerWidth = 360;
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginRight: -drawerWidth,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginRight: 0,
+    }),
+  }),
+);
+
+export default function Home(props) {
+  const [openSideBar, setOpenSideBar] = React.useState(false);
+
+  const onClickOpen = () => {
+      setOpenSideBar(true);
+  }
+
+  const onCloseOpen = () => {
+      setOpenSideBar(false);
+  }
   return (
     <div>
-      <Appbar/>
-      <DrawerLogin/>
+      <Appbar openSideBar={openSideBar} onClickOpen={onClickOpen} onCloseOpen={onCloseOpen}/>
+      <Main open={openSideBar}>
+        <DrawerLogin/>
+      </Main>
     </div>
   )
 }
